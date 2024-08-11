fe451969613143ad3cae4b87a5cfce47f6603691
Shashank Neupane <suryaneupane0427@gmail.com>
2024-08-11 13:12:44 +0545

    updated

--- data/users.js
+++
@@ -1,5 +0,0 @@
-export const users = [
-    { username: "tong", password: "tong123", page: "tong.html" },
-    { username: "sudeep", password: "sudeep123", page: "sudeep.html" },
-    { username: "admin", password: "admin", page: "admin.html" }
-  ];
